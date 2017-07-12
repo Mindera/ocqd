52306f23155b6494d0467df24dbbd208b64217b8
Sara Vieira <sara.vieira@mindera.com>
2017-07-12 16:11:25 +0100

    add win page

--- src/routes/win/index.js
+++ src/routes/win/index.js
@@ -1,13 +1,27 @@
 import { h, Component } from 'preact';
 import style from './style';
+import logo from '../../assets/logo.svg';
+import stars from '../../assets/stars.svg';
+import cup from '../../assets/cup.svg';
 
-export default class Win extends Component {
+class Win extends Component {
 	render() {
 		return (
-			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Win component.</p>
+			<div class={style.win}>
+				<img src={logo} class={style.logo} />
+				<img src={stars} class={style.stars} />
+				<img src={cup} class={style.cup} />
+
+
+				<div class={style.textContainer}>
+					<h1>Boa!!!!</h1>
+					<h1>
+						Adivinhaste  em 00:59
+					</h1>
+				</div>
 			</div>
 		);
 	}
 }
+
+export default Win;
